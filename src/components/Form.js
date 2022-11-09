c6fc8dc5b0103a13c3294262939645344e4fbea4
PoaceZerep <109180252+PoaceZerep@users.noreply.github.com>
2022-11-09 19:52:10 -0300

    Add files via upload

---
+++ src/components/Form.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+
+function Form() {
+    function cadastroUsuario(e) {
+        e.preventDefault()
+        console.log(`Usuário ${name} foi cadastrado com a senha: ${password}`)
+    }
+
+    const [name , setName] = useState()
+    const [password , setPassword] = useState()
+
+
+    return (
+        <div>
+            <h1>Meu cadastro:</h1>
+            <form onSubmit={cadastroUsuario}>
+                <div>
+                    <label htmlfor="name">Nome:</label>
+                    <input 
+                    type="text"
+                    id="name"
+                    name="name" 
+                    placeholder="Digite o seu nome" 
+                    onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlfor="password">Senha:</label>
+                    <input 
+                    type="password"
+                    id="password"
+                    name="password" 
+                    placeholder="Digite sua senha" 
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input type="submit" value="Cadastrar" />
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Form
---
+++ src/pages/Contato.js
@@ -0,0 +1,10 @@
+function Contato() {
+    return (
+        <div>
+          <h1>Contatos</h1>
+          <p>Conteudo da página</p>
+        </div>
+    )
+}
+
+export default Contato
